0e4eba34e7b9960425e91b4d91ba078773c88488
Jonathan Yee <jonathan.yee16@gmail.com>
2023-03-02 15:31:52 -0700

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'vue-restaurant-project',
   plugins: [vue()],
   resolve: {
     alias: {
