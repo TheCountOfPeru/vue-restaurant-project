b74e13fbee0b2a5cb1eaba747e469f06bf0987e5
Jonathan Yee <jonathan.yee16@gmail.com>
2023-03-04 16:07:01 -0700

    added extra route for home

--- src/router.js
+++ src/router.js
@@ -2,14 +2,10 @@ import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
   { path: "/home", component: () => import("./components/Home.vue") },
+  { path: "/vue-restaurant-project", component: () => import("./components/Home.vue") },
   { path: "/menu", component: () => import("./components/Menu.vue") },
   { path: "/gallery", component: () => import("./components/Gallery.vue") },
   { path: "/contact", component: () => import("./components/Contact.vue") },
-  // {
-  //   path: "/:catchAll(.*)",
-  //   name: "NotFound",
-  //   component: PageNotFound,
-  // },
 ];
 
 const router = createRouter({
